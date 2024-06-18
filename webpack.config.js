a1f836072b40002fc18c814d6f2256b70074e6e3
Vasil Vanchuk <v.vanchuk@tinkoff.ru>
2024-06-18 21:44:27 +0300

    Dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 9000,
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
