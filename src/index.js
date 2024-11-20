896b748499fe5601e0fbc17ef7e2e2fd9d1c0735
Vasil Vanchuk <v.vanchuk@tinkoff.ru>
2024-11-20 21:07:48 +0300

    Use basename to render links

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './router';
 import './styles.css';
+
+const header = document.createElement('header');
+header.innerHTML = `
+      <h1>Client-side URL change examples</h1>
+      <nav style="display: flex; justify-content: space-around">
+        <a href="${BASENAME}/">Home</a>
+        <a href="${BASENAME}/contacts">Contacts</a>
+        <a href="${BASENAME}/about">About</a>
+        <a href="${BASENAME}/about/us">About / Us</a>
+      </nav>
+`;
+
+document.body.prepend(header);
